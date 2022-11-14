9ea7c2adf814a5ed5dac66762d1729b85f646531
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-14 02:36:08 -0800

    eyeIcon added by using heroIcons

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './Questions.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Questions = ({questions}) => {
     const {correctAnswer, question, options} = questions;
     const notify = (checkedAnswer) => checkedAnswer === correctAnswer ? toast("Your answer is Correct!"): toast("Your answer is Wrong!");
     return (
         <div className='my-2 questions px-3 py-4 border rounded'>
-            <h5 className='text-start'>{question}</h5>
+            <div className='d-flex justify-content-between'>
+                <h5 className='text-start'>{question}</h5>
+                <div>
+                    <EyeIcon className="h-6 w-6 text-blue-700 fw-bolder"/>
+                    <p className='icon'>...</p>
+                </div>
+            </div>
+            
             {
                 options.map(option=>(
                     <div className='text-start'>
