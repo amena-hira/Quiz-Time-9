c0fcca46035c935b0cb6685495b3a7f1b82d42fb
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-14 03:33:32 -0800

    Statistics page responsive added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts';
+import './Statistics.css'
 const Statistics = () => {
     const topics = useLoaderData();
     const data = topics.data;
     console.log(topics.data);
     return (
-        <div className='mt-4 d-flex justify-content-center'>
-            <LineChart width={600} height={300} data={data}>
-                <Line type="monotone" dataKey="total" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="name" />
-                <YAxis />
-            </LineChart>
-        </div>
+        
+            <div className='mt-4 container'>
+                <ResponsiveContainer className='mx-auto' width="70%" height={300}>
+                    <LineChart data={data}>
+                        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                    </LineChart>
+                </ResponsiveContainer>
+                
+            </div>
+        
+        
     );
 };
 
