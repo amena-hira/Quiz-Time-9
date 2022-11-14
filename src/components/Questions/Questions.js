045f207953e9050269ccf03bb069f6989bb17030
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-14 02:47:20 -0800

    show correct answer in eyeIcon

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -7,25 +7,29 @@ import { EyeIcon } from '@heroicons/react/24/solid'
 const Questions = ({questions}) => {
     const {correctAnswer, question, options} = questions;
     const notify = (checkedAnswer) => checkedAnswer === correctAnswer ? toast("Your answer is Correct!"): toast("Your answer is Wrong!");
+    const notifyCorrectAnswer = () =>{
+        toast(`Correct Answer: ${correctAnswer}`);
+    }
     return (
         <div className='my-2 questions px-3 py-4 border rounded'>
             <div className='d-flex justify-content-between'>
-                <h5 className='text-start'>{question}</h5>
                 <div>
-                    <EyeIcon className="h-6 w-6 text-blue-700 fw-bolder"/>
+                    <h5 className='text-start'>{question}</h5>
+                    {
+                        options.map(option=>(
+                            <div className='text-start'>
+                                
+                            <input type="radio" className="form-check-input" id={option.id} name="optradio" value={option} onClick={()=>notify(option)}/> {option}  
+                            </div>
+                        ))
+                    }
+                </div>
+                
+                <div>
+                    <EyeIcon onClick={notifyCorrectAnswer} className="h-6 w-6 text-blue-700 fw-bolder"/>
                     <p className='icon'>...</p>
                 </div>
             </div>
-            
-            {
-                options.map(option=>(
-                    <div className='text-start'>
-                        
-                       <input type="radio" className="form-check-input" id={option.id} name="optradio" value={option} onClick={()=>notify(option)}/> {option}  
-                    </div>
-                ))
-            }
-            
             <ToastContainer autoClose={1000}/>
             
             
