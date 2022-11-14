96368a0d300cfedb517d8c2392ae7bf4a2878c42
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-13 19:51:50 -0800

    Header added with responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,14 @@ import './Header.css';
 const Header = () => {
     return (
 
-        <div className='container'>
-            <div className='header'>
-                <h2>We help you learn what you love</h2>
+        <div className='container mt-1'>
+            <div className='d-flex align-items-center img-fluid header py-5'>
+                <div className='text-white'>
+                    <h2 className=''>We help you learn what you love</h2>
+                    <p className='mx-auto text-center w-75'>A quiz refers to a short test of knowledge, typically around 10 questions in length, with question formats often including multiple choice, fill in the blanks, true or false and short answer. </p>
+                    <button className='p-2 rounded'>Browse Quizzes</button>
+                </div>
+                
             </div>
             
         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
 
             <nav className="navbar navbar-expand-lg ">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="#">
+                    <Link className="navbar-brand mx-auto" to="#">
                         <img className='img-fluid' src={Brand} alt="Logo" /> Quiz Time</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
